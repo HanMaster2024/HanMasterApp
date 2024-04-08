d97f79e2936e3ec26d76f280c7d9e7647a16a3dd
luoxuhong <barntet@163.com>
2024-04-09 00:25:38 +0800

    fix: contract connect

--- src/pages/wallet-connection/index.tsx
+++ src/pages/wallet-connection/index.tsx
@@ -6,30 +6,43 @@ import {
   IonToolbar,
   IonButtons,
   IonButton,
+  useIonLoading,
 } from "@ionic/react";
 import { useState } from "react";
-import Web3 from "web3";
+import { Connection, PublicKey } from "@solana/web3.js";
+// import contractABI from "./ContractABI.json";
 
 const connectWallet: React.FC = () => {
+  const [present, dismiss] = useIonLoading();
   const [connectedAccount, setConnectedAccount] = useState("null");
 
-  async function connectMetamask() {
-    //check metamask is installed
-    if (window.ethereum) {
-      console.log(window.ethereum);
-      // instantiate Web3 with the injected provider
-      const web3 = new Web3(window.ethereum);
+  async function connectContract() {
+    present({
+      message: "Connecting ...",
+    });
 
-      //request user to connect accounts (Metamask will prompt)
-      await window.ethereum.request({ method: "eth_requestAccounts" });
+    try {
+      const connection = new Connection("https://api.devnet.solana.com");
+      const contractAddress = "9JjymTUKnDM91mAx3g75yvMkxW8udrx16AfVkmRwjNjJ";
 
-      //get the connected accounts
-      const accounts = await web3.eth.getAccounts();
+      // 获取合约信息
+      const contractInfo = await connection.getAccountInfo(
+        new PublicKey(contractAddress)
+      );
 
-      //show the first connected account in the react page
-      setConnectedAccount(accounts[0]);
-    } else {
-      alert("Please download metamask");
+      if (contractInfo) {
+        console.log("Successfully connected to contract:", contractAddress);
+        console.log(contractInfo);
+        setConnectedAccount(contractInfo);
+        dismiss();
+        // 在此处可以执行其他逻辑，例如获取合约的其他信息
+      } else {
+        dismiss();
+        console.error("Failed to connect to contract:", contractAddress);
+      }
+    } catch (error) {
+      dismiss();
+      console.error("Error connecting to contract:", error);
     }
   }
 
@@ -40,17 +53,16 @@ const connectWallet: React.FC = () => {
           <IonButtons slot="start">
             <IonBackButton></IonBackButton>
           </IonButtons>
-          <IonTitle>wallet connection</IonTitle>
+          <IonTitle>contract connection</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent id="page-content-warp" class="ion-padding">
-        {/* Button to trigger Metamask connection */}
-        <IonButton onClick={() => connectMetamask()}>
-          Connect to wallet
+        <IonButton onClick={() => connectContract()}>
+          Connect to contract
         </IonButton>
 
         {/* Display the connected account */}
-        <h2>{connectedAccount}</h2>
+        <h2>{JSON.stringify(connectedAccount)}</h2>
       </IonContent>
     </>
   );
